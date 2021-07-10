5622c0a1a17a93c524374ba5d09825ec744582af
amirhosseinka <>
2021-07-10 13:15:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+from plugins.querys import get_admins
+import asyncio
+from time import sleep
+import os
+
+api_id = os.environ['api_id']
+api_hash = os.environ['api_hash']
+bot_token = os.environ['bot_token']
+
+app = Client(session_name='bot',
+             api_id=api_id,
+             api_hash=api_hash,
+             bot_token=bot_token)
+
+
+@app.on_message(filters.command("start") & filters.group)
+async def start(client, message: Message):
+    # await message.reply_text("Hello World!", reply_to_message_id=message.message_id)
+    await message.reply_text("""
+    سلام، ربات با موفقیت نصب شد.
+    برای دیدن دستور های ربات /help را بفرستید.
+    Digi Foss For Ever ✌
+    """)
+
+
+@app.on_message(filters.command("help") | filters.regex(r"^پ+ن+ل$"))
+async def help_menu(c, m: Message):
+    if get_admins(str(m.chat.id).strip("-"), m.from_user.id):
+        await m.reply_text(
+            """
+        سلام. به پنل راهنما خوش امدید.
+        برای اطلاع از دستور های ربات روی دکمه های زیر کلیک کنید.
+        """,
+            reply_markup=InlineKeyboardMarkup([
+                [
+                    InlineKeyboardButton(
+                        "مدیریت گروه",
+                        callback_data=
+                        f"grp_management,{m.from_user.id}"  # Group_management
+                    ),
+                    InlineKeyboardButton(
+                        'فان😜', callback_data=f"fun,{m.from_user.id}"),
+                    InlineKeyboardButton(
+                        "تنظیمات", callback_data=f"setting,{m.from_user.id}")
+                ],
+                [
+                    InlineKeyboardButton(
+                        "بستن پنل",
+                        callback_data=f"cls_pnl,{m.from_user.id}"  # Close_panel
+                    )
+                ]
+            ]))
+
+
+async def job():
+    await app.send_message(-1001154364334, "ّI'm up  /:")
+
+
+scheduler = AsyncIOScheduler()
+scheduler.add_job(job, "interval", seconds=300)
+
+scheduler.start()
+# خط های بالا برای روشن نگه داشتن ربات روی هروکو هست، اگه شما از هروکو استفده نمیکنید  میتونید کامنت کنید
+# یا اگه استفاده میکنید یه گروه بسازید و تو قسمت ارسال پیامش چت ایدی اون گروه رو قرار بدید
+app.run()
+
+app.run()
---
+++ plugins/create_db.py
@@ -0,0 +1,43 @@
+import sqlite3
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+def create_user(chat_id):
+    if not os.path.exists("/home/runner/DigiFoss/databases"): # for heroku
+        os.mkdir("/home/runner/DigiFoss/databases")
+    dbname = str(chat_id) + ".db"
+    con = sqlite3.connect("databases/" + dbname)
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS USERS 
+                    (name text, username text, num_id int NOT NULL unique, is_admin blob, warn int, status text, 
+                    is_verified blob)''')
+
+    con.commit()
+    con.close()
+
+
+def create_setting(chat_id):
+    dbname = str(chat_id) + ".db"
+    con = sqlite3.connect("databases/" + dbname)
+    cur = con.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS SETTING
+                (comment_protector blob, answer_why blob)
+    """)
+    cur.execute("INSERT INTO SETTING VALUES (?,?)", (True, False))
+    con.commit()
+    con.close()
+
+
+# chat = '-10328989782321'
+# create(chat)
+
+@Client.on_message(filters.command('Create'))
+async def db_create(c, m: Message):
+    ids = str(m.chat.id)
+    ids = ids.strip('-')
+    create_user(ids)
+    create_setting(ids)
+
+    await m.reply_text("دیتا بیس ایجاد شد !")
